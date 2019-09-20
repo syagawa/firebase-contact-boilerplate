08e4b08f65cead60b75f05ae204f2148db02eab7
syagawa <yagawa@qzc.co.jp>
2019-09-21 00:27:41 +0900

    Remove vue

--- main.js
+++ main.js
@@ -1,76 +1,69 @@
-import Vue from "vue";
 import firebase from "firebase";
 
 const makeContact = function(){
-  global.app = new Vue({
-    el: "#contact",
-    data: {
-      names: {
-        name: "名前",
-        ph_name: "名前",
-        email: "メールアドレス",
-        ph_email: "abc@example.com",
-        contents: "内容",
-        ph_contents: "お問い合わせ内容を入力してください"
-      },
-      messages: {
-        do_you_send: "問い合わせを送信しますか？",
-        complete: "お問い合わせありがとうございます。送信完了しました",
-        error: "送信に失敗しました。時間をおいて再度お試しください",
-        need_input: "項目を入力してください"
-      },
-      fbinit: "",
-      name: "",
-      email: "",
-      contents: ""
-    },
-    computed: {
+  if(!document.getElementById("contact")){
+    return;
+  }
 
-    },
-    methods: {
-      submitForm: function(){
-        return false;
-      },
-      sendMail: function(){
-        if(!this.email || !this.contents){
-          global.alert(this.messages.need_input);
-          return false;
-        }
+  const messages = {
+    do_you_send: "問い合わせを送信しますか？",
+    complete: "お問い合わせありがとうございます。送信完了しました",
+    error: "送信に失敗しました。時間をおいて再度お試しください",
+    need_input: "項目を入力してください"
+  };
 
-        const do_you_send = global.confirm(this.messages.do_you_send);
+  let fbinit = false;
 
-        if(!this.fbinit){
-          this.fbinit = firebase.initializeApp({
-            apiKey: 'set your api key',
-            authDomain: 'set your auth domain',
-            projectId: 'set your project id'
-          });
-        }
-        const sendobj = {
-          name: this.name,
-          email: this.email,
-          contents: this.contents
-        };
+  function sendMail(obj){
 
-        const mailer = firebase.functions().httpsCallable('sendMail');
-        mailer(sendobj)
-          .then(() => {
-            global.alert(this.messages.complete);
-          })
-          .catch(err => {
-            console.log(err);
-            global.alert(this.messages.error);
-          })
-          .finally(() => {
-            console.log("finally");
-          });
-
-      }
-
-    },
-    mounted: function(){
-
+    if(!obj.email || !obj.contents){
+      alert(messages.need_input);
+      return false;
     }
+
+    const do_you_send = confirm(messages.do_you_send);
+
+    if(!fbinit){
+      fbinit = firebase.initializeApp({
+        apiKey: 'set you api key',
+        authDomain: 'set you auth domain',
+        projectId: 'set you project id'
+      });
+    }
+    const sendobj = {
+      name: obj.name,
+      email: obj.email,
+      contents: obj.contents
+    };
+
+    const mailer = firebase.functions().httpsCallable('sendMail');
+    mailer(sendobj)
+      .then(() => {
+        global.alert(messages.complete);
+      })
+      .catch(err => {
+        console.log(err);
+        global.alert(messages.error);
+      })
+      .finally(() => {
+        console.log("finally");
+      });
+  }
+
+
+  document.getElementById("contact-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    const name = document.getElementById("input-name").value;
+    const email = document.getElementById("input-email").value;
+    const contents = document.getElementById("input-contents").value;
+
+    sendMail({name: name, email: email, contents: contents});
+
   });
 
+
 };
+
+makeContact();
